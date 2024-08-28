0a768cd5253d9ab9d982b6948a80bb55cd8fafef
YellowAfterlife <yellowafterlife@hotmail.com>
2024-08-28 14:15:49 +0300

    The cause of my problems is once again self-sabotage (wrong allocation size)

--- winwin/StringConv.h
+++ winwin/StringConv.h
@@ -10,30 +10,40 @@ public:
     StringConv() {
 
     }
+    void init() {
+        cbuf = nullptr;
+        cbuf_size = 0;
+        wbuf = nullptr;
+        wbuf_size = 0;
+    }
     LPWSTR wget(size_t size) {
         if (wbuf_size < size) {
-            wbuf = (wchar_t*)realloc(wbuf, size * sizeof(char));
+            if (wbuf != nullptr) {
+                wbuf = realloc_arr(wbuf, size);
+            } else wbuf = malloc_arr<wchar_t>(size);
             wbuf_size = size;
         }
         return wbuf;
     }
     LPCWSTR proc(const char* src, int cp = CP_UTF8) {
-        size_t size = MultiByteToWideChar(cp, 0, src, -1, NULL, 0);
-        LPWSTR buf = wget(size);
-        MultiByteToWideChar(cp, 0, src, -1, wbuf, (int)size);
+        auto size = MultiByteToWideChar(cp, 0, src, -1, NULL, 0);
+        LPWSTR buf = wget((size_t)size);
+        MultiByteToWideChar(cp, 0, src, -1, wbuf, size);
         return wbuf;
     }
     char* get(size_t size) {
         if (cbuf_size < size) {
-            cbuf = (char*)realloc(cbuf, size * sizeof(char));
+            if (cbuf != nullptr) {
+                cbuf = realloc_arr(cbuf, size);
+            } else cbuf = malloc_arr<char>(size);
             cbuf_size = size;
         }
         return cbuf;
     }
-    const char* proc(LPCWSTR src, int cp = CP_UTF8) {
-        size_t size = WideCharToMultiByte(cp, 0, src, -1, NULL, 0, NULL, NULL);
-        char* buf = get(size);
-        WideCharToMultiByte(cp, 0, src, -1, buf, (int)size, NULL, NULL);
+    char* proc(LPCWSTR src, int cp = CP_UTF8) {
+        auto size = WideCharToMultiByte(cp, 0, src, -1, NULL, 0, NULL, NULL);
+        char* buf = get((size_t)size);
+        WideCharToMultiByte(cp, 0, src, -1, buf, size, NULL, NULL);
         return buf;
     }
 };
--- winwin/winwin_props.cpp
+++ winwin/winwin_props.cpp
@@ -12,7 +12,7 @@ dllg uintptr_t winwin_get_handle(ww_ptr ww) {
 dllg const char* winwin_get_caption(ww_ptr ww) {
     auto len = GetWindowTextLengthW(ww->hwnd);
     if (len <= 0) return "";
-    wchar_t* ws = ww_c1.wget(len + 1);
+    auto ws = ww_c1.wget(len + 1);
     memset(ws, 0, sizeof(wchar_t) * (len + 1));
     GetWindowTextW(ww->hwnd, ws, len + 1);
     return ww_cc(ws);
